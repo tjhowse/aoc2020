9339f7714219d186b1b4dcbe38674f1426547691
Travis Howse <tjhowse@gmail.com>
2020-12-12 19:39:41 +1000

    Day 10 part 2 solution taken from reddit.

--- day10/day10.go
+++ day10/day10.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"log"
 	"os"
-	"runtime/pprof"
 	"sort"
 	"strconv"
 )
@@ -30,50 +29,13 @@ func fileToSlice(filename string) []string {
 	return contents
 }
 
-// func getCombo(array []int)
-
-func getLinkableIndices(index uint8, array []uint8) []uint8 {
-	linkables := []uint8{}
-	for i := index; i < uint8(len(array)); i++ {
-		jolt := array[i]
-		if jolt <= array[index] {
-			continue
+func contains(array []uint8, check uint8) bool {
+	for _, i := range array {
+		if i == check {
+			return true
 		}
-		if jolt-array[index] > 3 {
-			break
-		}
-		linkables = append(linkables, i)
 	}
-	return linkables
-}
-
-func do(result map[[100]uint8]struct{}, input []uint8, part seq, index uint8) int {
-	var total int
-	// m := make(map[[100]uint8]struct{})
-	part.a = append(part.a, input[index])
-	next := getLinkableIndices(index, input)
-	if len(next) == 0 {
-		var k [100]uint8
-		copy(k[:], part.a[:])
-		result[k] = struct{}{}
-		// m[k] = struct{}{}
-
-		if len(result) > 10000000 {
-			f, _ := os.Create("dump")
-			defer f.Close()
-			pprof.WriteHeapProfile(f)
-			log.Fatal("too big, ", len(result))
-		}
-
-		return len(result)
-	}
-	for _, i := range next {
-		// log.Print(part)
-		split := part
-		total += do(result, input, split, i)
-
-	}
-	return total
+	return false
 }
 
 func load() {
@@ -88,33 +50,46 @@ func load() {
 	}
 	dist := make(map[uint8]int)
 	sort.Slice(jolts, func(i, j int) bool { return jolts[i] < jolts[j] })
+	jolts = append(jolts, jolts[len(jolts)-1]+3)
 	// sort.Ints(jolts)
+	// log.Print(jolts)
 	for i := 0; i < len(jolts)-1; i++ {
 		// log.Print(i)
 		diff := jolts[i+1] - jolts[i]
+		// log.Print(diff)
 		if diff > 3 {
 			log.Fatal("Ohno")
 		}
 		dist[diff]++
 	}
-	// We start at zero
+	// We start at zero // I don't understand this.
 	dist[jolts[0]]++
-	// We end at +3 above the last one
-	dist[3]++
 	log.Print(dist[1] * dist[3])
 
-	m := make(map[[100]uint8]struct{})
-	var s seq
-	s.a = append(s.a, 0)
-	do(m, jolts, s, 0)
-	log.Print(len(m))
-	// for _, i := range jolts {
-	// 	log.Print(i)
-	// }
+	// https://old.reddit.com/r/adventofcode/comments/ka8z8x/2020_day_10_solutions/gfcxuxf
+	// I got stumped on this one, with many dead ends and much frustration. I looked up someone
+	// else's solution on reddit. I re-wrote it and go and understood how it worked, I think.
 
+	// This stores the number of ways of reaching this point in the sequence.
+	paths := make(map[uint8]int)
+	// There's only one way to reach the start.
+	paths[0] = 1
+	// For all joltages, starting at zero
+	for _, j := range jolts {
+		// Look at 1, 2 and 3 up from the current joltage
+		for i := 1; i < 4; i++ {
+			// If there's a joltage in our list that matches current + 1,2 or 3
+			if contains(jolts, j+uint8(i)) {
+				// Increase the count of ways of reaching that joltage by the number of
+				// ways we could reach the current joltage. Multiple paths to this joltage
+				// will increase its path count by their path counts.
+				paths[j+uint8(i)] += paths[j]
+			}
+		}
+	}
+	log.Print(paths[jolts[len(jolts)-1]])
 }
 
 func main() {
-	// runtime.SetBlockProfileRate(1)
 	load()
 }
